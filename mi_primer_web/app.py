2b8d4bcebb80367eb7d2d89ab5c5e974852d4c85
Joaquín Sorianello <joac@joac.com.ar>
2013-04-29 11:08:34 -0300

    Uso de form

--- mi_primer_web/app.py
+++ mi_primer_web/app.py
@@ -8,8 +8,14 @@ import bottle
 def index():
     return bottle.template('inicio.tpl')
 
-@bottle.route('/saludar/<nombre_del_usuario>')
-def saludar(nombre_del_usuario):
+@bottle.route('/saludar/<nombre_del_usuario>', method='GET')
+@bottle.route('/saludar', method='POST')
+def saludar(nombre_del_usuario=None):
+
+    #Obtenemos el nombre de la persona desde el request
+    if nombre_del_usuario == None:
+        nombre_del_usuario = bottle.request.forms.get('nombre')
+
     return bottle.template('saludar.tpl',
                             nombre=nombre_del_usuario,
                             otro_nombre="Jose")
