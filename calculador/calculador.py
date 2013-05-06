74e787c9db8e63e875e8d3e41a05883da581c906
Joaquín Sorianello <joac@joac.com.ar>
2013-05-05 22:40:31 -0300

    Leo el contenido del archivo

--- calculador/calculador.py
+++ calculador/calculador.py
@@ -7,8 +7,15 @@ def guardar_en_archivo(texto):
     archivo.write(texto + '\n')
     archivo.close()
 
+def leer_archivo():
+    archivo = open(nombre_archivo, 'r')
+    lineas = archivo.readlines()
+    archivo.close()
+    return lineas
+
 
 
 if __name__ == '__main__':
     guardar_en_archivo('Hola Mundo')
     guardar_en_archivo('Esta es otra linea')
+    print leer_archivo()
